16cb72f1ff988d75c6a701a6e8abfed219f3fb40
Romeer <rizziromanalejandro@gmail.com>
2014-11-08 14:50:42 -0300

    R.

--- memory/ContinuousAssignment.py
+++ memory/ContinuousAssignment.py
@@ -12,29 +12,24 @@ class ContinuousAssignment:
         self._policy = policy
 
     def create_new_block(self, pcb):
-            if self.exists_block_with_space(p):
+            if self.exists_block_with_space(pcb):
                 block_to_use = self._policy.find_block(self._free_blocks)
                 block_to_use.setUsed()
                 self._free_blocks.remove(block_to_use)
-                new_block_end_index = pcb.amount_of_instructions() - 1
-                new_block = Block(0, 0, new_block_end_index)
+                new_block = Block(0, 0, pcb.amount_of_instructions() - 1)
                 self.increase_blocks_ids()
-                self._blocks[0].changeStartIndex(self.new_block_end_index + 1)
+                self._blocks[0].changeStartIndex(pcb.amount_of_instructions())
                 self._blocks[0].changePreviousBlock_double(self.new_block)
-                self._blocks[0].decrease_size(self.new_block.size())
-                self._blocks.insert(0, self.new_block)
+                self._blocks[0].decrease_size(new_block.size())
+                self._blocks.insert(0, new_block)
                 self.update_free_blocks()
             else:
                 self.compact()
 
-    ''' ¿Es necesario o alcanza con solo mirar los bloques?'''
-    def enough_space_for(self, program):
-        return program.amount_of_instructions() < self._memory.get_free_space
-
-    def exists_block_with_space(self, program):
+    def exists_block_with_space(self, pcb):
         result = False
         for block in self._blocks:
-            if block.isFree() & (block.size() >= program.amount_of_instructions()):
+            if block.isFree() & (block.size() >= pcb.amount_of_instructions()):
                 result = True
         return result
 
