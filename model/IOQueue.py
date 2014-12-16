a045882665ff272df933019e1c74688daae0403e
Romeer <rizziromanalejandro@gmail.com>
2014-12-16 02:23:07 -0300

    R.

--- model/IOQueue.py
+++ model/IOQueue.py
@@ -16,12 +16,14 @@ class IOQueue(Thread):
 
     def dispatch(self):
         self._scheduler.add(self._waiting[0])
-        self._waiting.remove(0)
+        self._waiting.remove(self._waiting[0])
 
     def run(self):
         while True:
             self._lock.acquire()
             current_process = self._waiting[0]
-            while self._memory_manager.read(current_process.get_pc).isIO():
+            while self._memory_manager.read(current_process.get_pc()).isIO():
+                if current_process.has_finished():
+                    break
                 current_process.increment()
             self.dispatch()
